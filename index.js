67dbef101127a891bd3216620480e7c884227b48
Stephen Young <StephenJayYoung@gmail.com>
2014-09-24 10:33:45 -0700

    Some more tests (with wbyoung).

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function SiteGenerator() {
 }
 
 SiteGenerator.prototype.generateSite = function(a, b, cb) {
+	// use the generateFile method to make things easier!
+	setTimeout(cb, 0);
+};
+
+SiteGenerator.prototype.generateFile = function(a, b, c, cb) {
+	// use the generateFile method to make things easier!
 	setTimeout(cb, 0);
 };
 
--- test/test.js
+++ test/test.js
@@ -36,19 +36,48 @@ describe('site generator', function() {
 //TODO: Write some tests to make sure that the site generator: 1) Can read the file,
 // 2) Can find the place where the content is supposed to go, 3) Can find what needs to be replaced.
 //this test makes sure we can find the place where the content will go
+
+
 describe('combine files', function() {
 	it.skip('combines the content from 2 files into 1 new file', function(done) {
+		//it is going to expect the content to be a file with new content
+		var generator = new SiteGenerator();
+		var layout = path.join(__dirname, 'fixtures/site/layout.html');
+		var index = path.join(__dirname, 'fixtures/site/index.html');
+
+		var outputDirectory = path.join(__dirname, 'tmp');
+		var outputFile = path.join(outputDirectory, 'index.html');
+
+		var expectedDirectory = path.join(__dirname, 'fixtures/expected/site1');
+		var expectedFile = path.join(expectedDirectory, 'index.html');
+		console.log(expectedFile)
+
+		generator.generateFile(layout, index, outputFile, function(err) {
+			fs.readFile(outputFile, { encoding: 'utf8' }, function(err, outputContents) {
+				fs.readFile(expectedFile, { encoding: 'utf8' }, function(err, expectedContents) {
+					console.log(err)
+					expect(outputContents).to.eql(expectedContents);
+					done();
+				});
+			});
+		});
+	});
+});
+
+describe('generate a string', function() {
+	it.skip('takes input from 2 strings and returns 1 supercalifragilisticexpialidocious string', function(done) {
 	//it is going to expect the content to be a file with new content
 	var generator = new SiteGenerator();
-	var expectedSite1 = path.join(__dirname, 'fixtures/expected/site1');
-	var layout = path.join(_dirname, 'fixtures/site/layout.html');
-	var index = path.join(_dirname, 'fixtures/site/index.html');
-	var newFile = generator.generateSite(layout, index);      //combine(layoutFile, contentFile);
-
-	expect(newFile.length).to.eql(1);
+	var string1 = ("this is the combo of all of the {{contents}}");
+ 	var string2 = ('majestic things');
+ 	var expectedString = ('this is the combo of the majestic things');
+	var result = generator.combined(string1, string2);      //combine(layoutFile, contentFile);
+	expect(result).to.eql(expectedString);
 		//this is going to require the index file so that we can make sure that we can read it
 	});
 });
+
+
 
 
 
