0fbcba817fd996911cbd4c2fb76485564fbb4977
Stephen Young <StephenJayYoung@gmail.com>
2014-09-23 17:07:29 -0700

    whit took a loot at it and said COMMIT

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 var path = require('path');
 var fs = require('fs');
 var SiteGenerator = require('../index');
+var fileReader = require('../index');
 // need to input the name of the object that we are operating on
 
 
@@ -32,5 +33,35 @@ describe('site generator', function() {
 	});
 });
 
-//TODO: write some tests to make sure that the site generator: 1) Can read the file
-// 2)Can find the place where the content is supposed to go, 3) Can implement the
+//TODO: Write some tests to make sure that the site generator: 1) Can read the file,
+// 2) Can find the place where the content is supposed to go, 3) Can find what needs to be replaced.
+
+//this test makes sure that the generator can read the file
+describe('file reader', function() {
+	it.skip('reads the file', function(done) {
+		var reader = new fileReader(); // I added a new path for this one, not sure if needed :)
+		// this is going to require the index file so that we can make sure that we can read it
+		
+	});
+});
+
+//this test makes sure we can find the place where the content will go
+describe('lets find the content', function() {
+	it.skip('finds where the content will go', function(done) {
+		var reader = new fileReader();
+		//this is going to require the index file so that we can make sure that we can read it
+	});
+});
+
+//this test finds what content needs to be replaced
+describe('content replacement', function() {
+	it.skip('finds what content needs to be replaced', function(done) {
+		var reader = new fileReader();
+		//this is going to require the index file so that we can make sure that we can read it
+	});
+});
+
+
+
+
+
